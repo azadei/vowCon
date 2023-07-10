abe121e5aee98d7dce956404abbb342b69d9a2a7
azadei <azadei@code.edu.az>
2023-07-10 15:52:13 +0400

    1

---
+++ vowCon/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a string: ");
+        string input = Console.ReadLine();
+
+        int vowel = 0;
+        int conso = 0;
+
+        foreach (char c in input.ToLower())
+        {
+            if (Char.IsLetter(c))
+            {
+                if ("aeiou".Contains(c))
+                    vowel++;
+                else
+                    conso++;
+            }
+        }
+
+        Console.WriteLine("Vowels: " + vowel);
+        Console.WriteLine("Consonants: " + conso);
+    }
+}
